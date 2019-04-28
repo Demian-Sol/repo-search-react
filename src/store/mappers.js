0e5a9e23fb45d90cff0277bdcbea58548d542b80
Demian-Sol <demian_messages@mail.ru>
2019-04-28 19:50:48 +0700

    add results listing

--- src/store/mappers.js
+++ src/store/mappers.js
@@ -1,12 +1,13 @@
 export const mapResponseToResults = response => {
   const results = response.data.items.map(result => {
     const {
+      id,
       full_name: name,
       html_url: url,
       watchers_count: watchersCount,
       stargazers_count: stargazersCount,
     } = result;
-    return { name, url, watchersCount, stargazersCount };
+    return { id, name, url, watchersCount, stargazersCount };
   });
   return results;
 };
